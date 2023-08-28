2e691d31f70f1dd0a75afdbc72e8d39a4e8be2d1
samdaphbynet <lj8@hotmail.fr>
2023-08-28 23:49:08 +0200

    added default value to max_delay

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,8 +2,8 @@
 """
     Import task_wait_random from the "0-basic_async_syntax.py" and
     write an async routine called wait_n that takes in 2 int arguments
-    (in this order): n and max_delay. You will spawn task_wait_random n times with
-    the specified max_delay.
+    (in this order): n and max_delay. You will spawn task_wait_random n
+    times with the specified max_delay.
     wait_n should return the list of all the delays (float values). The list of
     the delays should be in ascending order without using sort() because of
     concurrency.
@@ -16,7 +16,7 @@ from typing import List, Optional
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
         The function creates multiple tasks that each wait for a
         random delay and returns a list of the
