31589b5ebeab7950e7e1e56221e205434bf552cd
samdaphbynet <lj8@hotmail.fr>
2023-08-28 11:11:50 +0200

    fixed pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,6 +3,8 @@
     type-annotated function add that takes a float a and a float b as
     arguments and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
     """ Return sum as a float"""
     return a + b
