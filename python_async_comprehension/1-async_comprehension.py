40ba1ad008babb553ec82d148393d0d304a28bf1
samdaphbynet <lj8@hotmail.fr>
2023-08-29 23:27:33 +0200

    added shebang line

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
     Import async_generator from the previous task and then write a coroutine
     called async_comprehension that takes no arguments.
