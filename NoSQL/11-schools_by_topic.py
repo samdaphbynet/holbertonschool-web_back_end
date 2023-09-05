fa888436f8c57cb1f459af3a1c0535f774635ec4
samdaphbynet <lj8@hotmail.fr>
2023-09-05 11:05:46 +0000

    Where can I learn Python?

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Python function that returns the list of school having a specific topic:
+    - Prototype: def schools_by_topic(mongo_collection, topic):
+    - mongo_collection will be the pymongo collection object
+    - topic (string) will be topic searched
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+        schools_by_topic:  returns the list of school having a specific topic
+    """
+    document = mongo_collection.find({ "topics": topic })
+    list_school = [i for i in document]
+    return list_school
