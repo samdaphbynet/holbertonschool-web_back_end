44f0e72f1b92377e8d751fed45926c2dddd6c4eb
samdaphbynet <lj8@hotmail.fr>
2023-09-07 16:49:34 +0000

    Simple pagination that find the correct indexes to paginate the dataset correctly and return the appropriate page of the dataset

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+    function named index_range that takes two integer arguments.
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+        function should return a tuple of size two containing a start index
+        and an end index corresponding to the range of indexes to return in
+        a list for those particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        index = index_range(page, page_size)
+        start = index[0]
+        end = index[1]
+
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
