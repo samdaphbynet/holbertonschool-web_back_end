c2f747fbf49bbc6c4b2a0a56852e3fc6d40374a3
samdaphbynet <lj8@hotmail.fr>
2023-09-12 23:22:39 +0200

    added coma to capita

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -2,8 +2,12 @@ export default function getBudgetObject(income, gdp, capita) {
     const budget = {
         income,
         gdp,
-        capita
+        capita,
     };
 
     return budget;
 }
+
+
+
+
