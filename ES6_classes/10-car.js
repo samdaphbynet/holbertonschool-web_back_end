922b2478c9cc7b5c84987010c416f47dd5698915
samdaphbynet <lj8@hotmail.fr>
2023-09-15 21:42:19 +0000

    Implement a class named Car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+
+class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    cloneCar() {
+        return new this.constructor(this._brand, this._motor, this._color)
+    }
+}
+
+export default Car
