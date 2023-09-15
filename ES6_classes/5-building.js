630a7e8064c96fe451f12d21c4ffdc2f2a78d707
samdaphbynet <lj8@hotmail.fr>
2023-09-15 08:48:05 +0200

    A building class as an abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+class Building {
+    constructor(sqft) {
+        if (this.constructor !== Building && typeof evacuationWarningMessage !== 'function') {
+            throw Error("Class extending Building must override evacuationWarningMessage")
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+}
+
+export default Building;
