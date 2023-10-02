8bea89ab06221ec6f19fe03e8b94b617b6863bec
samdaphbynet <lj8@hotmail.fr>
2023-10-02 11:12:37 +0000

    Using the prototype below (getFullResponseFromAPI(success) ) return a promise. The parameter is a boolean.

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export default function getResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        reject(new Error("the fake API is not working currently"));
+      }
+    });
+  }
