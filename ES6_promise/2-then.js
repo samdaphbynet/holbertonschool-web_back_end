8bfdd40ecb8a3ed68d3d0113ba1e956248bdd4f7
samdaphbynet <lj8@hotmail.fr>
2023-10-02 12:06:35 +0000

    added return to promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 /* eslint disable */
 
 export default function handleResponseFromAPI(promise) {
-    promise
+    return promise
         .then((response) => {
             console.log("Got a response from the API");
             return { status: 200, body: 'success' };
