b3684c7052833bcb6e479cd32e030020ef149539
samdaphbynet <lj8@hotmail.fr>
2023-10-02 12:03:38 +0000

    update

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -4,10 +4,10 @@ export default function handleResponseFromAPI(promise) {
     promise
         .then((response) => {
             console.log("Got a response from the API");
-            return { status: 200, body: 'Success' };
+            return { status: 200, body: 'success' };
         })
         .catch(() => {
             console.log("Got a response from the API");
             return new Error();
-        })
+        });
 }
