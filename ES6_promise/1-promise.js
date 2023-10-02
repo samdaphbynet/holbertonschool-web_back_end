718f3e85e67143995f392e2e05bf735d394fdf90
samdaphbynet <lj8@hotmail.fr>
2023-10-02 11:14:34 +0000

    correct message Error

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getResponseFromAPI(success) {
       if (success) {
         resolve({ status: 200, body: 'Success' });
       } else {
-        reject(new Error("the fake API is not working currently"));
+        reject(new Error("The fake API is not working currently"));
       }
     });
   }
