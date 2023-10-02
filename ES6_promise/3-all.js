7e0acf992a32d09f2ca95cab844e6b6a98aabc23
samdaphbynet <lj8@hotmail.fr>
2023-10-02 13:31:27 +0000

    add the event of an error, log Signup system offline to the console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const first = await uploadPhoto();
-  const second = await createUser();
+  try {
+    const first = await uploadPhoto();
+    const second = await createUser();
 
-  console.log(`${first.body} ${second.firstName} ${second.lastName}`);
+    console.log(`${first.body} ${second.firstName} ${second.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
