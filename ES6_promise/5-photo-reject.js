031ffaa6c415678f9c1e527f2ecdea76f3466a35
samdaphbynet <lj8@hotmail.fr>
2023-10-02 18:29:36 +0000

    change syntex of the Promise

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,7 +1,5 @@
 // 5-photo-reject.js
 
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => {
-    reject(`${fileName} cannot be processed`);
-  });
+  return Promise.reject(new Error(`Error: ${fileName} cannot be processed`));
 }
