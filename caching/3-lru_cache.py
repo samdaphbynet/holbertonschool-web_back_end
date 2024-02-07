559b4d86259473cedc409a79ad9fdf053926b95a
samdaphbynet <lj8@hotmail.fr>
2024-02-07 19:02:45 +0100

    3. LRU Caching

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" 3-lru_cache """
+
+
+from collections import OrderedDict
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class """
+    def __init__(self):
+        """ Initialize LRUCache """
+        super().__init__()
+        self.usage_order = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is None or item is None:
+            return
+
+        # Remove the least recently used item
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if self.usage_order:
+                lru_key = next(iter(self.usage_order))
+                del self.cache_data[lru_key]
+                del self.usage_order[lru_key]
+                print("DISCARD: {}".format(lru_key))
+            else:
+                lru_key = next(iter(self.cache_data))
+                del self.cache_cache[lru_key]
+                print("DISCARD: {}".format(lru_key))
+
+        self.cache_data[key] = item
+        self.usage_order[key] = None
+
+    def get(self, key):
+        """ get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
