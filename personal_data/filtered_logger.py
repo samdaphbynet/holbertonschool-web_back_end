3fcb651906e32101892553cbf4c9f111e3da3f28
samdaphbynet <lj8@hotmail.fr>
2024-02-19 12:33:04 +0100

    added annotation of all arguments in the function filter_datum

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,9 +5,19 @@ function called filter_datum that returns the log message obfuscated
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Arguments:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating
+        all fields in the log line (message)
+    """
     for field in fields:
         message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
     return message
