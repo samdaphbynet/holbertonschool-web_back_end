28ea1874c8ae5087df4704b847c3096592fe77e2
samdaphbynet <lj8@hotmail.fr>
2024-02-19 12:40:00 +0100

    fixed pycodeatyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -20,5 +20,5 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
-                         field + "=" + redaction + separator,message)
+                         field + "=" + redaction + separator, message)
     return message
