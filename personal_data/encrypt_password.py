aecbb47266832c6f4debb93837f800a8feba316a
samdaphbynet <lj8@hotmail.fr>
2024-02-20 18:28:03 +0100

    added type of argument passed in the function

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ User passwords should NEVER be stored in plain text in a database.
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """
     hash_password function that expects one string argument name password and
     returns a salted, hashed password, which is a byte string.
