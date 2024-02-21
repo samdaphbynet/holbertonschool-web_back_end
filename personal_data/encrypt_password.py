a31eb47a4afbca05e5ca46f5543f2e5ffb1b3e4f
samdaphbynet <lj8@hotmail.fr>
2024-02-21 09:38:35 +0100

    delete encode "utf-8"

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -22,4 +22,4 @@ def is_valid(hashed_passsword: bytes, password: str) -> bool:
         hashed_password: bytes type
         password: string type
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_passsword)
+    return bcrypt.checkpw(password.encode(), hashed_passsword)
