f34f76463d0e8a11a670fcab9896720b99c34f4a
samdaphbynet <lj8@hotmail.fr>
2024-02-22 18:33:25 +0100

    documented the function extract....

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,9 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        function to extract the base64 encoded authorization header
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
