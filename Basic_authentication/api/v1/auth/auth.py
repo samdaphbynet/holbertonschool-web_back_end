2d94574e0940d1106b076333928e708a8a4f2996
samdaphbynet <lj8@hotmail.fr>
2024-02-22 12:40:25 +0100

    class auth to manage the api authontication

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        handler to get the authorization header
+        """
+        if (request is None):
+            return None
+
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Validates the current user
+        """
+        return None
