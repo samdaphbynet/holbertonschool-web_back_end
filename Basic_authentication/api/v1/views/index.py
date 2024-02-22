9e42b65880d69047e0228c9ccd18ded58c1b97cc
samdaphbynet <lj8@hotmail.fr>
2024-02-22 11:50:42 +0100

    testing this new error handler, add a new endpoint forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -32,3 +33,12 @@ def unauthorized() -> str:
       - raises a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route("forbidden", methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    Get /api/v1/forbidden
+    Return: raises a 403 error by using abort
+    """
+    abort(403)
