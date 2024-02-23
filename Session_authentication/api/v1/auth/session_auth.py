469422a450dd74f89ddfbe40b79651862b4e9431
samdaphbynet <lj8@hotmail.fr>
2024-02-23 10:46:28 +0100

    class that inherits from Auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+class SessionAuth that inherits from class Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    class to manage the session authentication
+    """
+    pass
