107d9bb3acf1be01a218fad4157483b0073c70f1
samdaphbynet <lj8@hotmail.fr>
2024-02-25 19:55:26 +0100

    update function @app.before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -42,11 +42,12 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None \
-        and auth.session_cookie(request) is None:
+    if not auth.authorization_header(request) \
+            and not auth.session_cookie(request):
         abort(401)
 
     request.current_user = auth.current_user(request)
+
     if auth.current_user(request) is None:
         abort(403)
 
