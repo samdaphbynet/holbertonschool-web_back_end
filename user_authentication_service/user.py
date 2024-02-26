c82e744063860c20b8523efbfd1806df766b5d87
samdaphbynet <lj8@hotmail.fr>
2024-02-26 11:07:28 +0100

    correct column hashed_password

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -17,6 +17,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hached_password = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
