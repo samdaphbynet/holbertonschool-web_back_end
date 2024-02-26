43fa7d238e2baa7cbf089e4673895995562e26e5
samdaphbynet <lj8@hotmail.fr>
2024-02-26 11:42:44 +0100

    change name of variable user to new_user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -34,8 +34,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
 
-        return user
+        return new_user
