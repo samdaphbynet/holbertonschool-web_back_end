c872f1e0a7f48833c48a0312c33d10ffed0f1ce7
samdaphbynet <lj8@hotmail.fr>
2024-02-26 13:43:03 +0100

    added function 2. Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -39,3 +41,20 @@ class DB:
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        function to find a first user in the database
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound("No user found")
+
+            print("=============++>", user.id)
+            return user
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
