d1cc903f82c753c07581a0db202f31eb036533c0
samdaphbynet <lj8@hotmail.fr>
2024-02-27 14:05:27 +0000

    function:  email and password string arguments and return a User object.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,32 @@
 Hash password
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        function to register a new user in the database
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hash = _hash_password(password)
+            user = self._db.add_user(email, hash)
+
+            return user
+
+        else:
+            raise ValueError(f"User {email} already exists")
 
 
 def _hash_password(password: str) -> str:
