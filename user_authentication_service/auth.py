cb4621269a53c0791da37f2ab0095aeb8fafe5e6
samdaphbynet <lj8@hotmail.fr>
2024-02-27 13:40:29 +0000

    method that takes in a password string arguments and returns bytes

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Hash password
+"""
+
+
+import bcrypt
+
+
+def _hash_password(password) -> bytes:
+    """
+    function to hash password using sha256 algorithm
+    """
+
+    hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    return hash
