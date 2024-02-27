f0ebab54ad6ed10295ebed553e5751fe0d965178
samdaphbynet <lj8@hotmail.fr>
2024-02-27 18:02:16 +0000

    8. Credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -30,6 +30,17 @@ class Auth:
         else:
             raise ValueError(f"User {email} already exists")
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        function to validate a user's login credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> str:
     """
