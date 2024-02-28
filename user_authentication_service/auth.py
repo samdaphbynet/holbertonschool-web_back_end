91378bcf343df04858b73378a3c9152045075ef9
samdaphbynet <lj8@hotmail.fr>
2024-02-28 12:00:58 +0000

    12. Find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -55,6 +55,17 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
+    def get_user_from_session_id(self, session_id):
+        """
+        function to get a user from a session id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
+
 
 def _generate_uuid() -> str:
     """
