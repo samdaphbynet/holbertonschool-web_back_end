d3fd675ef56b31f1c3bdc71a4088c71d67bb02f1
samdaphbynet <lj8@hotmail.fr>
2024-02-29 11:43:51 +0000

    added function to generate a reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,6 +79,19 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        function to generate a reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        token = _generate_uuid()
+        self._db.update_user(user.id, token=token)
+        return token
+
 
 def _generate_uuid() -> str:
     """
