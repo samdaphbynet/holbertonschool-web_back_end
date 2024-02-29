2523102ce153f10ad17eda52ec518799e7ee76b7
samdaphbynet <lj8@hotmail.fr>
2024-02-29 11:35:45 +0000

    delete type None

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -60,10 +60,8 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> None:
-    """
-    function that responds to a logout request
-    """
+def logout():
+    """ function that responds to a logout request """
     session_id = request.cookies.get('session_id')
     user_id = AUTH.get_user_from_session_id(session_id)
 
