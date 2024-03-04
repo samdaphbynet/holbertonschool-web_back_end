a90b130dc9fa5ed4ba20838192f4806c148bd367
samdaphbynet <lj8@hotmail.fr>
2024-03-04 18:20:36 +0000

    fixed pycodestyle

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -10,6 +10,7 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config():
     """
     Config class for Babel
@@ -17,6 +18,7 @@ class Config():
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 @app.route("/")
 def index():
