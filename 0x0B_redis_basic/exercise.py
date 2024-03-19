cc73eb883ea9f7412b58d3b70ca8f06ba57cf7c3
samdaphbynet <lj8@hotmail.fr>
2024-03-19 11:51:24 +0000

    1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -28,3 +28,26 @@ class Cache():
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: callable = None)\
+            -> Union[str, int, bytes, float]:
+        """
+        function that return the string reading
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Union[str, bytes]:
+        """
+        function that return the string reading from the specified key
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[str, bytes]:
+        """
+        function that automatically parametrize
+        """
+        return self.get(key, fn=lambda d: int(d))
