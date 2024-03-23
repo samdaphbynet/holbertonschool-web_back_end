4a9dc9ff4f3c11a00ed68408e57e5efdaa4e754b
samdaphbynet <lj8@hotmail.fr>
2024-03-23 17:52:12 +0100

    Executing basic javascript with Node JS

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// This is a function named 'displayMessage' which takes in one parameter 'message'
+function displayMessage(message) {
+  // The function logs the 'message' parameter to the console
+  console.log(message);
+}
+
+module.exports = displayMessage;
