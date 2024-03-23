aebbf50007d29e9810b6e968f7d91702bc2fc654
samdaphbynet <lj8@hotmail.fr>
2024-03-23 18:04:40 +0100

    delete the comment

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,4 @@
-// This is a function named 'displayMessage' which takes in one parameter 'message'
 function displayMessage(message) {
-  // The function logs the 'message' parameter to the console
   console.log(message);
 }
 
