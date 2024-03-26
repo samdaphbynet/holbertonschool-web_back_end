1edf0250662bb54728fe867362eb1beac996d7f6
samdaphbynet <lj8@hotmail.fr>
2024-03-26 13:46:42 +0100

    added extention

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./1-calcul.js");
 
 describe("calculateNumber", function () {
   describe("SUM operation", function () {
